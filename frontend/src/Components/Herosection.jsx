1b2868c12fb4637428ad0e6b4644f8bd4e633a2e
rafboah <rafboah@gmail.com>
2023-11-04 15:39:42 -0400

    Created Herosection.jsx and Herosection.modules.css files

---
+++ frontend/src/Components/Herosection.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import styles from "../Static/Herosection.modules.css";
+
+const Herosection = () => {
+
+    return(
+
+        <div className={styles.Herosection}>
+            <h5>Welcome To The</h5>
+            <h5>AA</h5>
+            <h5>All-American</h5>
+            <h5>Airlines!</h5>
+        </div>
+    )
+}
+
+export default Herosection;
