1446b1bf3fb5cd63f603963ae1dc1b6fbe140c70
Adelina Kachinda <96156467+AdelinaKachinda@users.noreply.github.com>
2023-11-04 17:08:47 -0500

    main page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,14 @@
 // import logo from './logo.svg';
 import './App.css';
 import HomePage from './Screens/HomePage';
-
 import QueueStatus from './Components/QueueStatus';
 
 function App() {
   return (
 
     <div className="App">
-      <Header/>
+      <HomePage />
+    
     </div>
   );
 }
--- frontend/src/Components/Main.jsx
+++ frontend/src/Components/Main.jsx
@@ -16,5 +16,3 @@ export const Main = () => {
 
 export default Main;
 
-// blue screen
-// plane
