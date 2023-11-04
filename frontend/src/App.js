2b7cae8d78695dbfeede6c4ae1d950fea3ed6300
mulengangulale <ngulalemulenga@gmail.com>
2023-11-04 16:48:11 -0500

    Created queue status

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,14 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 // import HomePage from './Screens/HomePage';
 
+import QueueStatus from './Components/QueueStatus';
 
 function App() {
   return (
 
     <div className="App">
-      <HomePage />
-      
-
+      <Header/>
     </div>
   );
 }
---
+++ frontend/src/Components/QueueStatus.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import queuestyle from "../Static/Queuestatus.module.css"
+
+const QueueStatus = () => {
+  return (
+    <div className={queuestyle.Herosection}>
+        <div className='row'>
+            <div className='col-xl-6 col-lg-6 col-md-6 col-sm-4'>
+            </div>
+        </div>
+        <h1>This is the queue page </h1>
+    </div>
+  )
+}
+
+export default QueueStatus
