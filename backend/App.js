814f1e6c2ac72d8775c5ec2563756642c0d74ad0
mulengangulale <ngulalemulenga@gmail.com>
2023-11-04 14:40:46 -0500

    Connected to the Mongo DB database

--- backend/App.js
+++ backend/App.js
@@ -2,13 +2,27 @@ const express = require("express") ;
 const bodyParser = require("body-parser");
 const cors = require("cors");
 const app = express();
+const mongoose = require("mongoose");
+const UserModel = require("./models/Users")
+
+mongoose.connect("mongodb+srv://flightDB:allamerican@cluster0.8qri3sr.mongodb.net/flightDB?retryWrites=true&w=majority");
+
+// app.use(express.json());
+
 
 // Handling GET / request 
 app.get("/", (req, res, next) => { 
 	res.send("This is the express server by Mulenga") 
 }) 
 
-// Handling GET /hello request 
+app.get("/getUsers", (req, res)=>{
+	UserModel.find({}).then(function(users) {
+		res.json(users)
+	}).catch(function(err) {
+		res.json(err)
+	})
+})
+
 app.get("/virtual-queue", (req, res, next) => { 
 	res.send("This is the virtual queue page"); 
 }) 
