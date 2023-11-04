758e0fc4567bb97137116e1ccecb8ed2c7cb24f2
rafboah <rafboah@gmail.com>
2023-11-04 17:39:57 -0400

    Created and edited  Header.module.css file

--- frontend/src/Screens/HomePage.jsx
+++ frontend/src/Screens/HomePage.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import Herosection from '../Components/Herosection'
 import Header from '../Components/Header'
+import styles from './Homepage.module.css'
 
 
 const HomePage = () => {
 
   return (
-    <div>
+    <div className={styles.Homepage}>
    
       <Header />
       <Herosection />
