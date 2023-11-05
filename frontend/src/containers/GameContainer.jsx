7bd709d1a5ffa2ad6fe0ac3fba04a9d3158b2f11
Adelina Kachinda <96156467+AdelinaKachinda@users.noreply.github.com>
2023-11-05 07:47:48 -0600

    fixed quiz game

--- frontend/src/containers/GameContainer.jsx
+++ frontend/src/containers/GameContainer.jsx
@@ -10,11 +10,16 @@ const GameContainer = () => {
   const [showResult, setShowResult] = useState(false);
 
   const handleAnswerSubmit = () => {
-      if (userAnswer.toLowerCase() === questions[currentQuestion].answer.toLowerCase()) {
-          setScore(score + 1);
-          setShowResult('Correct!');
+      const question = questions[currentQuestion];
+      if (userAnswer && question && question.answer) {
+          if (userAnswer.toLowerCase() === question.answer.toLowerCase()) {
+              setScore(score + 1);
+              setShowResult('Correct!');
+          } else {
+              setShowResult('Wrong answer');
+          }
       } else {
-          setShowResult('Wrong answer');
+          console.error('Unable to check answer. User answer or question is undefined.');
       }
   };
 
@@ -32,17 +37,21 @@ const GameContainer = () => {
   return (
     <div className="min-h-screen flex items-center justify-center bg-gray-900 text-white font-ubuntu">
       <div className="text-center">
-        <h2 className="text-4xl mb-4">{`${questions[currentQuestion].question}`}</h2>
-        <input
-          type="text"
-          value={userAnswer}
-          onChange={(e) => setUserAnswer(e.target.value)}
-          placeholder="Enter True or False"
-          className="bg-white text-black rounded-lg p-2 mb-4"
-        />
-        <button className="bg-blue-500 text-white rounded-lg p-2 mr-2" onClick={handleAnswerSubmit}>
-          Submit
-        </button>
+        {questions[currentQuestion] && (
+          <>
+            <h2 className="text-4xl mb-4">{`${questions[currentQuestion].question}`}</h2>
+            <input
+              type="text"
+              value={userAnswer}
+              onChange={(e) => setUserAnswer(e.target.value)}
+              placeholder="Enter True or False"
+              className="bg-white text-black rounded-lg p-2 mb-4"
+            />
+            <button className="bg-blue-500 text-white rounded-lg p-2 mr-2" onClick={handleAnswerSubmit}>
+              Submit
+            </button>
+          </>
+        )}
         {showResult && <p className="mb-4">{showResult}</p>}
         <p className="mb-4">Score: {score}</p>
         <button className="bg-blue-500 text-white rounded-lg p-2" onClick={handleNextQuestion}>
@@ -52,11 +61,6 @@ const GameContainer = () => {
       <img className='w-100 h-100 absolute bottom-10 left-1/2 transform -translate-x-1/2 object-contain' src={plane} alt="Plane"/>
     </div>
   );
-  
-  
-
-
 };
-
 
 export default GameContainer;
