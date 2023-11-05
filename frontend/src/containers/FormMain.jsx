8375318b7f1ebf8e6c36ad4948e17c393cb4c6b2
rafboah <rafboah@gmail.com>
2023-11-05 01:49:35 -0500

    Created and Edited FormMain.jsx file

--- frontend/src/containers/FormMain.jsx
+++ frontend/src/containers/FormMain.jsx
@@ -14,9 +14,10 @@ const FormMain = () => {
                     <label htmlFor="flightNumber">Flight #</label><br/>
                     <input type="text" id="flightNumber" name="flightNumber" className="bg-gray-400 rounded w-full h-8" /><br/>
                     <label htmlFor="concern">Concern Description</label><br/>
-                    <input type="text" id="concern" name="concern" className="bg-gray-400 rounded w-full h-8" /><br/>
-                    <input type="submit" value="Join" className="bg-american-blue-2 p-2"/>
+                    <input type="text" id="concern" name="concern" className="bg-gray-400 rounded w-full h-8" />
+                    <input type="submit" value="Join" className="bg-american-blue-2 py-3 px-5 mt-5"/>
                     </div>
+                    <img className='w-100 h-100 absolute bot-0 object-contain' src={plane} alt="Plane"/>
                 </form>
             </div>
         </div>
