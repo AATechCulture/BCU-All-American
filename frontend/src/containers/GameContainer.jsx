404a96fbac91d13b3905b409a63c263a265bcf9a
Adelina Kachinda <96156467+AdelinaKachinda@users.noreply.github.com>
2023-11-05 07:57:14 -0600

    quiz game

--- frontend/src/containers/GameContainer.jsx
+++ frontend/src/containers/GameContainer.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import questions from '../data/questions.js';
 
-const plane = require("../resources/plane.png")
+const plane = require("../resources/plane.png");
 
 const GameContainer = () => {
   const [currentQuestion, setCurrentQuestion] = useState(0);
@@ -10,28 +10,27 @@ const GameContainer = () => {
   const [showResult, setShowResult] = useState(false);
 
   const handleAnswerSubmit = () => {
-      const question = questions[currentQuestion];
-      if (userAnswer && question && question.answer) {
-          if (userAnswer.toLowerCase() === question.answer.toLowerCase()) {
-              setScore(score + 1);
-              setShowResult('Correct!');
-          } else {
-              setShowResult('Wrong answer');
-          }
+    const question = questions[currentQuestion];
+    if (userAnswer && question && question.answer) {
+      if (userAnswer.toLowerCase() === question.answer.toLowerCase()) {
+        setScore(score + 1);
+        setShowResult('Correct!');
       } else {
-          console.error('Unable to check answer. User answer or question is undefined.');
+        setShowResult('Wrong answer');
       }
+    } else {
+      console.error('Unable to check answer. User answer or question is undefined.');
+    }
   };
 
   const handleNextQuestion = () => {
-      if (currentQuestion + 1 < questions.length) {
-          setCurrentQuestion(currentQuestion + 1);
-          setShowResult(false);
-          setUserAnswer('');
-      } else {
-          // End of the game
-          setShowResult(`Game Over! Your final score is ${score}`);
-      }
+    if (currentQuestion + 1 < questions.length) {
+      setCurrentQuestion(currentQuestion + 1);
+      setShowResult(false);
+      setUserAnswer('');
+    } else {
+      setShowResult(`Game Over! Your final score is ${score}`);
+    }
   };
 
   return (
@@ -39,7 +38,7 @@ const GameContainer = () => {
       <div className="text-center">
         {questions[currentQuestion] && (
           <>
-            <h2 className="text-4xl mb-4">{`${questions[currentQuestion].question}`}</h2>
+            <h2 className="text-4xl mb-4">{questions[currentQuestion].question}</h2>
             <input
               type="text"
               value={userAnswer}
@@ -54,11 +53,13 @@ const GameContainer = () => {
         )}
         {showResult && <p className="mb-4">{showResult}</p>}
         <p className="mb-4">Score: {score}</p>
-        <button className="bg-blue-500 text-white rounded-lg p-2" onClick={handleNextQuestion}>
-          Next Question
-        </button>
+        {questions[currentQuestion] && (
+          <button className="bg-blue-500 text-white rounded-lg p-2" onClick={handleNextQuestion}>
+            Next Question
+          </button>
+        )}
       </div>
-      <img className='w-100 h-100 absolute bottom-10 left-1/2 transform -translate-x-1/2 object-contain' src={plane} alt="Plane"/>
+      <img className='w-100 h-100 absolute bottom-10 left-1/2 transform -translate-x-1/2 object-contain' src={plane} alt="Plane" />
     </div>
   );
 };
